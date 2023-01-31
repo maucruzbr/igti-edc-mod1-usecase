26f66a82e80524f0931e0f7d5d164466575cf21e
Maurício Cruz <cruzm.br@outlook.com>
2023-01-30 23:00:28 -0300

    Corrigido nome do bucket de streaming

--- infraestructure/s3.tf
+++ infraestructure/s3.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
 
 
 resource "aws_s3_bucket" "stream" {
-  bucket = "igti-edc-streaming-bucket"
+  bucket = "igti-edc-streaming-bucket-mbr"
 
   tags = {
     IES   = "IGTI",
