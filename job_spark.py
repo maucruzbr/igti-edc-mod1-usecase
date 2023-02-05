d171bc38384259c5e6c48a990fb1b73420d55930
maucruzbr <cruzm.br@outlook.com>
2023-02-05 12:59:40 -0300

    comentado job spark

--- job_spark.py
+++ job_spark.py
@@ -1,27 +1,27 @@
-from pyspark.sql import SparkSession
-
-spark = (
-    SparkSession.builder.appName('ExerciseSpark')
-    .getOrCreate()
-)
-
-# Ler os dados do ENEM 2020
-enem = (
-    spark
-    .read
-    .format('csv')
-    .option('header', True)
-    .option('inferSchema', True)
-    .option('delimiter', ';')
-    .load('s3://edc-igti-datalake1/raw-data/enem/')
-)
-
-
-(
-    enem
-    .write
-    .mode('overwrite')
-    .format('parquet')
-    .partitionBy('enem')
-    .save('s3://edc-igti-datalake1/staging/enem')
-)
+# from pyspark.sql import SparkSession
+#
+# spark = (
+#     SparkSession.builder.appName('ExerciseSpark')
+#     .getOrCreate()
+# )
+#
+# # Ler os dados do ENEM 2020
+# enem = (
+#     spark
+#     .read
+#     .format('csv')
+#     .option('header', True)
+#     .option('inferSchema', True)
+#     .option('delimiter', ';')
+#     .load('s3://edc-igti-datalake1/raw-data/enem/')
+# )
+#
+#
+# (
+#     enem
+#     .write
+#     .mode('overwrite')
+#     .format('parquet')
+#     .partitionBy('enem')
+#     .save('s3://edc-igti-datalake1/staging/enem')
+# )
